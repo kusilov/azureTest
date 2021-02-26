dd41a4e2e4fa35d7f91bafc5edec02f0da11b226
Georhii Kasilov <egorkakasilov@gmail.com>
2021-02-26 13:29:57 +0200

    test

--- sampleapp-ios-swift/AppDelegate.swift
+++ sampleapp-ios-swift/AppDelegate.swift
@@ -16,12 +16,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Enabled App Center SDK verbose logs
         MSAppCenter.setLogLevel(.verbose)
         
-        MSAppCenter.start("<APP SECRET HERE>", withServices: [
+        MSAppCenter.start("8574c052-012f-4356-8c0f-0d94f81d28c7", withServices: [
             MSAnalytics.self,
             MSCrashes.self,
             MSPush.self,
         ])
         
+        
+        testAPI()
+        
         return true
+    }
+    
+    private func testAPI() {
+        let request = NSMutableURLRequest(url: NSURL(string: "https://restcountries-v1.p.rapidapi.com/alpha/?codes=co%3Bnorqwe")! as URL,
+                                                cachePolicy: .useProtocolCachePolicy,
+                                            timeoutInterval: 10.0)
+        request.httpMethod = "BLA"
+//        request.allHTTPHeaderFields = headers
+
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+            if (error != nil) {
+                print(error)
+            } else {
+                let httpResponse = response as? HTTPURLResponse
+                print(httpResponse)
+            }
+        })
+
+        dataTask.resume()
     }
 }
